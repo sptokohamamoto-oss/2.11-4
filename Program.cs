802e4ab62d154bdd697c89afed698777f0ed7fcc
冬桜子 濱元 <toko0124615@icloud.com>
2026-01-09 16:30:53 +0900

    a

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+
+    static void Main(string[] args)
+    {
+        string userid = "admin";
+        string password = "password123";
+
+
+        if (userid == "admin" && password == "password123")
+        {
+
+            Console.WriteLine("ログイン成功");
+
+        }
+        else 
+        {
+            Console.WriteLine("不正アクセス");
+
+        }
+
+    }
+     
+
+}
+
+
